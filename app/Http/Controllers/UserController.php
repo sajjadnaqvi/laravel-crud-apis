40b12b4f19b3258ab6601bd8eb13603c7e92a9c4
Sajjad Naqvi <sajjadmehdi1214@gmail.com>
2023-06-19 19:10:01 +0500

    Adds route to create user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Traits\ResponseTrait;
+use http\Client\Response;
+use http\Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    //
+    use ResponseTrait;
+
+    public function createUser(Request $request)
+    {
+        try {
+            $validatedData = $request->validate([
+                'email' => 'email|required|unique:users',
+                'password' => 'required',
+                'first_name' => 'required|max:55',
+                'last_name' => 'required|max:55',
+                'phone' => 'required'
+            ]);
+            $validatedData['password'] = Hash::make($request->password);
+            $user = User::create($validatedData);
+            return $this->responseSuccess($user);
+        } catch (Exception $exception) {
+            return $this->responseError(null, $exception->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('register',[AuthController::class,'register']);
 Route::post('login',[AuthController::class,'login']);
+
+Route::post('user',[UserController::class, 'createUser'])->middleware('auth:api');
