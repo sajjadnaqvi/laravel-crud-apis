5825e4d65d1ba281aed128f15f15f87fcd6631a7
Sajjad Naqvi <sajjadmehdi1214@gmail.com>
2023-06-19 18:52:52 +0500

    Adds route to register user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required',
+            'confirm_password' => 'required'
+        ]);
+        $user = User::create($data);
+
+        if ($user && auth()->attempt($data)) {
+            $accessToken = auth()->user()->createToken('authToken')->accessToken;
+            return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+        }
+    }
 
 }
