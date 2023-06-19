abc09130c106d0878aee87e8653b4045779b36e1
Sajjad Naqvi <sajjadmehdi1214@gmail.com>
2023-06-19 18:59:24 +0500

    Adds traits to handle response

---
+++ app/Traits/ResponseTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+
+trait ResponseTrait
+{
+    public function responseSuccess($data, $message = "Successful", $status_code = JsonResponse::HTTP_OK): JsonResponse
+    {
+        return response()->json([
+            'data'    => $data,
+        ], $status_code);
+    }
+
+    public function responseError($errors, $message = 'Data is invalid', $status_code = JsonResponse::HTTP_BAD_REQUEST): JsonResponse
+    {
+        return response()->json([
+            'status'  => false,
+            'message' => $message,
+            'errors'  => $errors,
+            'data'    => null,
+        ], $status_code);
+    }
+
+
+}
